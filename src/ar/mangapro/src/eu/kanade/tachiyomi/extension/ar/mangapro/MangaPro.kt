cf7f652bcf7113a59f6926adb0feed06eccd793e
alrmoh7 <alrawahimohammed7@gmail.com>
2026-01-17 17:45:22 +0400

    Change MangaPro URLs to new domain
    Updated the base URL and API URL for MangaPro.

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/MangaPro.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/MangaPro.kt
@@ -5,8 +5,8 @@ import eu.kanade.tachiyomi.multisrc.iken.Iken
 class MangaPro : Iken(
     "Manga Pro",
     "ar",
-    "https://promanga.net",
-    "https://api.promanga.net",
+    "https://www.prochan.net",     // تم تحديث الرابط هنا
+    "https://api.prochan.net",     // تم تحديث رابط الـ API (افتراضاً يتبع نفس النطاق)
 ) {
     override val versionId = 4
 }
